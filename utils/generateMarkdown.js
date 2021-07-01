136a105a993e425dd9c0862143b85d6fb5100784
Samantha Malone <sammac99@gmail.com>
2021-07-01 11:55:03 -0700

    adding sections to markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,6 +3,7 @@
 function badge(data) {
 const licenseType = data.license[0];
 // badge information
+// TODO: Create a function that returns the license link
 let licenseString = " "
 if (licenseType === 'MIT'){
   licenseString === `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
@@ -16,38 +17,34 @@ if (licenseType === 'GPL 3.0'){
 if (licenseType === 'BSD3'){
   licenseString === ``
 };
+// // If there is no license, return an empty string
 if (licenseType === 'None'){
-  licenseString === ``
+  licenseString === `![License: 'None'}](http://img.shields.io/badge/License-None)`
 };
 return licenseString
 }
-// TODO: Create a function that returns the license link
-![License: ${(data.license)data.license 'None'}](http://img.shields.io/badge/License-${(data.lic) ? data.license : 'None'})
-// // If there is no license, return an empty string
-function renderLicenseLink(license) {
-Repo by ${data.name}
-// // TODO: Create a function that returns the license section of README
-${data.description}
-}
-// // If there is no license, return an empty string
-function renderLicenseSection(license) {
----
-Installation:
-${data.installation}
-}
 // // TODO: Create a function to generate markdown for README add template literals, add table of contents
-Usage:
-${data.use}
 function generateMarkdown(data) {
-__Contribution:__
+  return `# ${data.name}
+
+## Description
+${data.description}
+---
+## Installation:
+${data.installation}
+
+## Usage:
+${data.use}
+
+## __Contribution:__
 ${data.contributors}
 
-__Tests:__
+## __Tests:__
 ${data.test}
 
-__Questions:__
+## __Questions:__
 ${data.questions}
   return `# ${data.title}
-}`
+}
 
 module.exports = generateMarkdown;
